4f5ae90023f3a88f938af90055cacd10b46d2dfd
AnGgIt88 <jarbull87@gmail.com>
2021-08-23 05:36:24 +0700

    test push in telegram

--- build.sh
+++ build.sh
@@ -2,24 +2,54 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://gitlab.com/OrangeFox/Manifest.git"
-DEVICE=rosy
-BRANCH=android-9.0
-DT_LINK="https://github.com/AnGgIt88/ofx_recovery_xiaomi_rosy -b $BRANCH"
-DT_PATH=device/xiaomi/$DEVICE
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
 
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+
+# Inlined function to post a message
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+
+tg_post_build() {
+	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
+	-F chat_id="$TG_CHAT_ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3"
+}
+
+# Send a notificaton to TG
+tg_post_msg "<b>$LLVM_NAME: Recovery Compilation Started</b>%0A<b>Date : </b><code>$rel_friendly_date</code>%0A<b>Toolchain Script Commit : </b><code>$builder_commit</code>%0A"
+
+tg_post_msg "<b>===+++ Setting up Build Environment +++===</b>"
 echo " ===+++ Setting up Build Environment +++==="
 apt-get install openssh-server -y
 apt-get update --fix-missing
 apt-get install openssh-server -y
 mkdir ~/ofox && cd ~/ofox
 
+tg_post_msg "<b>===+++ Syncing Recovery Sources +++===</b>"
 echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST
 repo sync
 repo sync
 git clone --depth=1 $DT_LINK $DT_PATH
 
+tg_post_msg "<b>===+++ Starting Build Recovery +++===</b>"
 echo " ===+++ Building Recovery +++==="
 export ALLOW_MISSING_DEPENDENCIES=true
 . build/envsetup.sh
@@ -29,8 +59,23 @@ echo " lunch omni_${DEVICE}-eng done"
 mka recoveryimage || abort " mka failed with exit status $?"
 echo " mka recoveryimage done"
 
+# Fin Error
+    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT_ID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+tg_post_msg "<b>===+++ Uploading Recovery +++===</b>"
 echo " ===+++ Uploading Recovery +++==="
 
-cd out/target/product/$DEVICE
-curl -sL https://git.io/file-transfer | sh && ./transfer wet *.zip
+# Push Recovery to channel
+    cd out/target/product/$DEVICE
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+        -F chat_id="$TG_CHAT_ID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>$DEVICE</b>"
