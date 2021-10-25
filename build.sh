0cecd17548f9da4fd852bd135d645de8ddea0f45
Finix-Projetcs <jarbull87@gmail.com>
2021-10-25 07:30:35 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -34,9 +34,6 @@ tg_post_build() {
 
 # Send a notificaton to TG
 tg_post_msg "<b>Test Building XiaomiParts Apps...</b>%0A<b>DATE : </b><code>$DATE</code>%0A"
-
-tg_post_msg "<b>===+++ Setting up Build Environment +++===</b>"
-echo " ===+++ Setting up Build Environment +++==="
 mkdir ~/xdroid && cd ~/xdroid
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
@@ -44,23 +41,15 @@ apt-get install openssh-server -y
 apt-get update --fix-missing
 apt-get install openssh-server -y
 
-tg_post_msg "<b>===+++ Syncing Sources +++===</b>"
-echo " ===+++ Syncing Rom Sources +++==="
 repo init --depth=1 -u https://github.com/xdroid-CAF/xd_manifest -b eleven
 git clone https://github.com/AnGgIt88/local_manifest.git --depth=1 -b eleven .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-
-tg_post_msg "<b>===+++ Starting Build +++===</b>"
-echo " ===+++ Building Rom +++==="
 export ALLOW_MISSING_DEPENDENCIES=true
 export KBUILD_BUILD_USER=xiaomi
 export KBUILD_BUILD_HOST=Finix-server
 . build/envsetup.sh
+lunch xdroid_rosy-userdebug
 mmma /device/xiaomi/rosy/XiaomiParts
-
-# Upload zips & Rom.img (U can improvise lateron adding telegram support etc etc)
-tg_post_msg "<b>===+++ Uploading +++===</b>"
-echo " ===+++ Uploading +++==="
 
 # Push Rom to channel
     cd out/target/product/rosy/system/priv-app/XiaomiParts
