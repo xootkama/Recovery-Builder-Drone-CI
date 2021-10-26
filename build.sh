a278446556bbb2a16797e3f8b96170b81fc6e1e3
Finix-Projetcs <jarbull87@gmail.com>
2021-10-26 17:36:12 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -38,6 +38,8 @@ mkdir ~/Projects && cd ~/Projects
 apt-get -y update && apt-get -y upgrade
 apt-get -y install openssh-server screen python git openjdk-8-jdk android-tools-adb bc bison build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev lib32readline-dev lib32z1-dev  liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev libtinfo5 libncurses5
 apt-get update --fix-missing
+wget https://storage.googleapis.com/git-repo-downloads/repo -P /usr/local/sbin/
+chmod +x /usr/local/sbin/repo
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
 
